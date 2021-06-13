54ac1333752941189a6ff7794399d3950da4e8cc
Emil Hristov <emodavitkov@abv.bg>
2021-06-13 22:58:11 +0300

    Go

---
+++ OOP_Polymorphism/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP_Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
